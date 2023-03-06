6de8bc5808bcff31755c87f1a33ae2972c2a3ed9
SamB440 <sam@islandearth.net>
2023-03-06 16:14:15 +0000

    Use shop items and direct link code

--- src/main/java/org/empirewar/lemonsqueezer/LemonSqueezer.java
+++ src/main/java/org/empirewar/lemonsqueezer/LemonSqueezer.java
@@ -5,8 +5,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.empirewar.lemonadestand.event.KoFiTransactionEvent;
+import org.empirewar.lemonadestand.kofi.ShopItem;
+import org.empirewar.lemonadestand.kofi.ShopOrder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -29,7 +32,21 @@ public final class LemonSqueezer extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onShop(KoFiTransactionEvent event) {
-        for (String command : getConfig().getStringList("purchase.commands")) {
+        final ShopOrder shopOrder = event.getShopOrder();
+        for (ShopItem shopItem : shopOrder.getShopItems()) {
+            final String code = shopItem.getDirectLinkCode();
+            List<String> commands = getConfig().getStringList("purchase.shop." + code);
+            for (int i = 0; i < shopItem.getQuantity(); i++) {
+                processCommands(event, commands);
+            }
+        }
+
+        final boolean subscriptionPayment = shopOrder.isSubscriptionPayment();
+        //todo: subscriptions
+    }
+
+    private void processCommands(KoFiTransactionEvent event, List<String> commandList) {
+        for (String command : commandList) {
             for (String key : VARIABLE_MAPPINGS.keySet()) {
                 String varialised = "{" + key + "}";
                 final Function<KoFiTransactionEvent, String> function = VARIABLE_MAPPINGS.get(key);
