b303493a7bc459aefc45c35096e3d80977fd625a
StealWonders <6114858+StealWonders@users.noreply.github.com>
2024-05-07 12:59:29 +0200

    deez sponge port is weird

---
+++ common/build.gradle.kts
@@ -0,0 +1,7 @@
+//repositories {
+//    maven("https://repo.convallyria.com/snapshots")
+//}
+
+dependencies {
+    implementation("org.empirewar.lemonadestand:LemonadeStand:1.0.0-SNAPSHOT")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "LemonSqueezer"
+include("common", "paper", "sponge")
